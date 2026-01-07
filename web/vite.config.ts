8b79130aba7fee0673685864a2ce1c5967bc72a3
Ali Safari <asafarim.it@gmail.com>
2026-01-07 19:48:07 +0100

    configure GitHub Pages deployment with base path

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: process.env.BASE_PATH || '/',
+    base: process.env.BASE_PATH || '/direxpo/',
     plugins: [react()],
     server: {
         port: 5198,
